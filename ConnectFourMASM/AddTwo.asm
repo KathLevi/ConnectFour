4ff0af78155f5e2e890dd4e1bd8109fa94d51650
Kathleen Levi <levi.kathleen@gmail.com>
2016-12-01 10:15:22 -0800

    NEW Assembler files to write functions.
    No link between C++ and Assembler yet,. Only the definition of functions that we are going to write in Assembler to import to C++.

---
+++ ConnectFourMASM/AddTwo.asm
@@ -0,0 +1,43 @@
+TITLE Connect Four Assembler Code
+
+;Program Description: Functions written in assembler that will be linked to our C++ main function
+;Author:	Kathleen, Andrew, Ryan, Jusice
+;Creation Date:		12/1/2016
+;Latest Revision:
+
+
+; 32-bit assembly language template
+
+INCLUDE Irvine32.inc
+
+.data
+
+.code
+main proc
+
+	call exitProcess
+main endp
+
+;FUNCTION TO DISPLAY BOARD
+displayBoard PROC
+	
+
+displayBoard endp
+
+;FUNCTION TO PLACE TILE
+placeTile PROC
+
+
+placeTile endp
+
+;FUNCTION TO EMPTY BOARD
+emptyBoard PROC
+
+
+emptyBoard endp
+
+;FUNCTION TO CHECK FOR A WINNER
+check PROC
+
+check endp
+end main
