117b5a46c8c390ccdeaefbd34f17aa40e4c22579
Kathleen Levi <levi.kathleen@gmail.com>
2016-12-02 14:20:40 -0800

    Wrote functions for emptyBoard and placeTile
    Both functions still need to pass in two-dimensional array for editing (similar to displayBoard function)

---
+++ ConnectFourMASM/ConnectFour.asm
@@ -0,0 +1,135 @@
+TITLE Connect Four Assembler Code
+ 
+;Program Description: Functions written in assembler that will be linked to our C++ main function
+;Author:       Kathleen, Andrew, Ryan, Jusice
+;Creation Date:                 12/1/2016
+;Latest Revision:
+ 
+; 32-bit assembly language template
+ 
+INCLUDE Irvine32.inc
+;----------------------------DATA----------------------------------------
+.data
+blank BYTE " ",0
+redBlue BYTE 14h            ;red text on blue background
+yellowBlue BYTE 30h       ;yellow text on blue background
+whiteBlue BYTE 31h                   ;white text on blue background
+blueBlue BYTE 17h                     ;blue text on blue background
+
+
+;-------------------------------MAIN FUNCTION------------------------------------- 
+.code
+main proc
+	call displayBoard
+ 
+	call exitProcess
+main endp
+
+;---------------------------------FUNCTION TO DISPLAY BOARD----------------------------------- 
+displayBoard PROC USES EAX ECX
+	mov eax, DWORD PTR blueBlue
+	call settextcolor
+	call Clrscr
+ 
+	mov ecx, 6
+	Loop4:
+		call row
+	loop Loop4 
+	ret
+displayBoard endp
+
+;-------------------------------FUNCTION TO CREATE A ROW OF THE BOARD-------------------------------------
+row PROC USES EAX ECX           ;outputs a row of boxes using ascii characters
+	mov ecx, 7
+	Loop1:                                          ;ouputs first third of a row of 7 boxes
+		mov al, 218
+		call Writechar
+		mov al, 196
+		call Writechar
+		mov al, 191
+		call Writechar
+		mov al, blank
+		call Writechar
+	loop Loop1
+	call crlf
+ 
+	mov ecx, 7
+	Loop2:                                          ;ouputs second third of a row of 7 boxes
+		mov al, 179
+		call Writechar
+	;set color for blank space based on player
+		mov al, blank
+		call Writechar
+		mov eax, DWORD PTR blueBlue                    ;set color back after outputting blank space
+		call settextcolor
+		mov al, 179
+		call Writechar
+		mov al, blank
+		call Writechar
+	loop Loop2
+	call crlf
+ 
+	mov ecx, 7
+	Loop3:                                          ;ouputs last third of a row of 7 boxes
+		mov al, 192
+		call Writechar
+		mov al, 196
+		call Writechar
+		mov al, 217
+		call Writechar
+		mov al, blank
+		call Writechar
+	loop Loop3
+	call crlf
+	ret
+row endp
+ 
+;--------------------------------FUNCTION TO PLACE TILE------------------------------------
+placeTile PROC USES ECX                     ;(int col, char player)
+	;DOES THIS WORK?? WILL ECX CHANGE WHILE I AM RUNNING THE CMP AND JMP STATEMENTS?
+	mov ecx, -1
+ 
+	;if (col <= 0 || col > 7 || board[0][col] != ' ')
+	cmp col, 0
+		jb isfalse
+	cmp col, 7
+		ja isfalse
+	cmp board[0][col], DWORD PTR blank      
+		jne isfalse
+	ret -1 ;return -1
+          
+	isfalse:         ;else  
+		;for ( i=0; board[i][col] == ' '; i++)
+		inc ecx
+		board[ecx][col], DWORD PTR blank
+		je isfalse
+			;if (i == 5)
+			cmp ecx, 5
+			je setTile		
+		dec ecx						;i--
+	setTile:
+		mov board[ecx][col], player	;board[i][col] = player
+		mov eax, ecx
+		ret						;return i	
+placeTile endp
+
+;-------------------------------FUNCTION TO EMPTY BOARD-------------------------------------
+emptyBoard PROC USES ECX
+    mov ecx, 6
+    push ecx
+    Loop1:
+        mov ecx, 7
+        Loop 2:        
+            mov board[i][j], DWORD PTR blank ;board[i][j] = ' '
+        loop Loop2
+    pop ecx
+    loop Loop1
+    ret
+emptyBoard endp
+
+;---------------------------------FUNCTION TO CHECK FOR A WINNER-----------------------------------
+check PROC
+ 
+    ret
+check endp
+end main
