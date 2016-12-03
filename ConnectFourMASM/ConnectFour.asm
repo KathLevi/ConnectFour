9cb6bd9c7bddd5ce05eff9c9e92e7ad9c864fb52
Kathleen Levi <levi.kathleen@gmail.com>
2016-12-03 14:51:04 -0800

    Added more functions :D

--- ConnectFourMASM/ConnectFour.asm
+++ ConnectFourMASM/ConnectFour.asm
@@ -6,17 +6,32 @@ TITLE Connect Four Assembler Code
 ;Latest Revision:			  12/02/2016
  
 ; 32-bit assembly language template
+;----------------------------REQUIREMENTS----------------------------------------
+;	Main Menu: 1) How many players (human vs. human OR human vs. computer)
+;				2) See stats from games played during THAT runtime
+;				3) exit game
+;	Blank game board when starting a new game	
+;	In order to win one must have 4 tiles in a row
+;	Turns alternate between players
+;	Keeps track of wins and loses for each game
+;	Give the user the option to exit after each game
+;	Quick exit (use alt + f4)
+;	Placing tiles:
+;		1) click in a space to place a tile
+;		2) tiles fall from veiling to place they are set
+;		3) use arrows to select column (use gotoxy)
+;--------------------------------------------------------------------------------
+
  
 INCLUDE Irvine32.inc
 ;----------------------------DATA----------------------------------------
 .data
+
 p1Prompt DWORD "Player 1: What would you like to be known as?",0
 p2Prompt DWORD "Player 2: What would you like to be known as?",0
 buff DWORD 80
 p1Name	DWORD buff+1 DUP(?)
 p2Name	DWORD buff+1 DUP(?)
-
-again DWORD "y",0
 
 blank BYTE " ",0
 redBlue BYTE 14h		;red text on blue background
@@ -28,6 +43,33 @@ blueBlue BYTE 17h		;blue text on blue background
 ;-------------------------------MAIN FUNCTION------------------------------------- 
 .code
 main proc
+	call askName
+
+BeginGame:	
+	call mainMenu
+
+	;if eax == 1
+		;call playGame
+		je BeginGame
+	;if eax == 2
+		;call instructions
+		je BeginGame
+	;if eax == 3
+		;call displayStats
+		je BeginGame
+	;if eax == 4
+		je EndGame
+	;if eax == 5
+		;output invaldMM
+		je BeginGame
+	
+ EndGame:
+	;call endGameMsg
+	call exitProcess
+main endp
+
+;---------------------------------FUNCTION TO ASK USER NAME-----------------------------------
+askName PROC USES EAX EDX ECX
 	mov eax, p1Prompt		;asks P1 name
 	call WriteString
 	mov edx, offset p1Name	;reads P1 name
@@ -40,12 +82,37 @@ main proc
 	mov ecx, buff
 	call ReadString
 
-	call Clrscr
-	call emptyBoard
-	call displayBoard
- 
-	call exitProcess
-main endp
+	ret
+askName endp
+
+;---------------------------------FUNCTION TO DISPLAY MAIN MENU-----------------------------------
+mainMenu PROC
+
+	ret
+mainMenu endp
+
+;---------------------------------FUNCTION TO PLAY GAME-----------------------------------
+playGame PROC
+	Call clrscr
+	Call emptyBoard
+	Call displayBoard
+
+	ret
+playGame endp
+
+;-------------------------------FUNCTION TO EMPTY BOARD-------------------------------------
+emptyBoard PROC USES ECX
+    mov ecx, 6
+    push ecx
+    Loop1:
+        mov ecx, 7
+        Loop 2:        
+            mov board[i][j], DWORD PTR blank ;board[i][j] = ' '
+        loop Loop2
+    pop ecx
+    loop Loop1
+    ret
+emptyBoard endp
 
 ;---------------------------------FUNCTION TO DISPLAY BOARD----------------------------------- 
 displayBoard PROC USES EAX ECX
@@ -113,11 +180,11 @@ placeTile PROC USES ECX                     ;(int col, char player)
  
 	;if (col <= 0 || col > 7 || board[0][col] != ' ')
 	cmp col, 0
-		jb isfalse
-	cmp col, 7
 		ja isfalse
+	cmp col, 7
+		jb isfalse
 	cmp board[0][col], DWORD PTR blank      
-		jne isfalse
+		je isfalse
 	ret -1 ;return -1
           
 	isfalse:         ;else  
@@ -135,23 +202,25 @@ placeTile PROC USES ECX                     ;(int col, char player)
 		ret						;return i	
 placeTile endp
 
-;-------------------------------FUNCTION TO EMPTY BOARD-------------------------------------
-emptyBoard PROC USES ECX
-    mov ecx, 6
-    push ecx
-    Loop1:
-        mov ecx, 7
-        Loop 2:        
-            mov board[i][j], DWORD PTR blank ;board[i][j] = ' '
-        loop Loop2
-    pop ecx
-    loop Loop1
-    ret
-emptyBoard endp
-
 ;---------------------------------FUNCTION TO CHECK FOR A WINNER-----------------------------------
 check PROC
  
     ret
 check endp
+
+;---------------------------------FUNCTION TO UPDATE STATS-----------------------------------
+updateStats PROC
+	;if player == 15
+		;inc p1score
+	;else
+		;inc p2score
+    ret
+updateStats endp
+
+;---------------------------------FUNCTION TO DISPLAY STATS-----------------------------------
+displayStats PROC
+	;name of p1 --> scorePrompt --> p1score
+	;name of p2 --> scorePrompt --> p2score
+    ret
+displayStats endp
 end main
