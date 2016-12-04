7fe13d25f22423b8cc6e992c64c1f6b54e7ec720
Andrew Gazeley <Andrew Gazeley@DESKTOP-1VR32OA>
2016-12-04 12:58:40 -0800

    Worked on case handling for the windows forms UI

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        private bool isPlayerOne = true;
         public Form1()
         {
             InitializeComponent();
@@ -28,27 +29,49 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
             bool hasControl = false;
             int i = 6;
             while (hasControl == false && i >= 0)
             {
-               
-                
+                if (isPlayerOne)
+                {
+
                     if (tableLayoutPanel1.GetControlFromPosition(0, i) == null)
                     {
-                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(), 0, i);
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 0, i);
                         hasControl = true;
+
+                        isPlayerOne = false;
+
                     }
                     else
                     {
                         i--;
                     }
-                
+                }
+                else
+                {
+                    isPlayerOne = true;
+                    if (tableLayoutPanel1.GetControlFromPosition(0, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 0, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+
+
+
             }
         }
-
-        private Label makeNewLabel()
+        private Label makeNewLabel(bool isPlayerOne)
         {
             Label label1 = new Label();
             // 
@@ -57,137 +80,289 @@ namespace WindowsFormsApplication1
             label1.AutoSize = true;
             label1.Cursor = System.Windows.Forms.Cursors.Default;
             label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F,FontStyle.Regular);
-            label1.ForeColor = Color.Crimson;
+            
             label1.Location = new System.Drawing.Point(4, 1);
             label1.Name = "label1";
             label1.Size = new System.Drawing.Size(60, 63);
             label1.TabIndex = 3;
             label1.Text = "O";
+            if (isPlayerOne)
+            {
+                label1.ForeColor = Color.Crimson;
+            }
+            else
+            {
+                label1.ForeColor = Color.Gold;
+            }
             return label1;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+
             bool hasControl = false;
             int i = 6;
             while (hasControl == false && i >= 0)
             {
-
-
-                if (tableLayoutPanel1.GetControlFromPosition(1, i) == null)
+                if (isPlayerOne)
                 {
-                    this.tableLayoutPanel1.Controls.Add(makeNewLabel(), 1, i);
-                    hasControl = true;
+
+                    if (tableLayoutPanel1.GetControlFromPosition(1, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 1, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
                 else
                 {
-                    i--;
+                    isPlayerOne = true;
+                    if (tableLayoutPanel1.GetControlFromPosition(1, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 1, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
+
+
 
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+
             bool hasControl = false;
             int i = 6;
             while (hasControl == false && i >= 0)
             {
-
-
-                if (tableLayoutPanel1.GetControlFromPosition(2, i) == null)
+                if (isPlayerOne)
                 {
-                    this.tableLayoutPanel1.Controls.Add(makeNewLabel(), 2, i);
-                    hasControl = true;
+
+                    if (tableLayoutPanel1.GetControlFromPosition(2, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 2, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
                 else
                 {
-                    i--;
+                    isPlayerOne = true;
+                    if (tableLayoutPanel1.GetControlFromPosition(2, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 2, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
+
+
 
             }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+
             bool hasControl = false;
             int i = 6;
             while (hasControl == false && i >= 0)
             {
-
-
-                if (tableLayoutPanel1.GetControlFromPosition(3, i) == null)
+                if (isPlayerOne)
                 {
-                    this.tableLayoutPanel1.Controls.Add(makeNewLabel(), 3, i);
-                    hasControl = true;
+
+                    if (tableLayoutPanel1.GetControlFromPosition(3, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 3, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
                 else
                 {
-                    i--;
+                    isPlayerOne = true;
+                    if (tableLayoutPanel1.GetControlFromPosition(3, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 3, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
+
+
 
             }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+
             bool hasControl = false;
             int i = 6;
             while (hasControl == false && i >= 0)
             {
-
-
-                if (tableLayoutPanel1.GetControlFromPosition(4, i) == null)
+                if (isPlayerOne)
                 {
-                    this.tableLayoutPanel1.Controls.Add(makeNewLabel(), 4, i);
-                    hasControl = true;
+
+                    if (tableLayoutPanel1.GetControlFromPosition(4, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 4, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
                 else
                 {
-                    i--;
+                    isPlayerOne = true;
+                    if (tableLayoutPanel1.GetControlFromPosition(4, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 4, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
+
+
 
             }
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
+
             bool hasControl = false;
             int i = 6;
             while (hasControl == false && i >= 0)
             {
-
-
-                if (tableLayoutPanel1.GetControlFromPosition(5, i) == null)
+                if (isPlayerOne)
                 {
-                    this.tableLayoutPanel1.Controls.Add(makeNewLabel(), 5, i);
-                    hasControl = true;
+
+                    if (tableLayoutPanel1.GetControlFromPosition(5, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 5, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
                 else
                 {
-                    i--;
+                    isPlayerOne = true;
+                    if (tableLayoutPanel1.GetControlFromPosition(5, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 5, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
+
+
 
             }
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
+
             bool hasControl = false;
             int i = 6;
             while (hasControl == false && i >= 0)
             {
-
-
-                if (tableLayoutPanel1.GetControlFromPosition(6, i) == null)
+                if (isPlayerOne)
                 {
-                    this.tableLayoutPanel1.Controls.Add(makeNewLabel(), 6, i);
-                    hasControl = true;
+
+                    if (tableLayoutPanel1.GetControlFromPosition(6, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 6, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
                 else
                 {
-                    i--;
+                    isPlayerOne = true;
+                    if (tableLayoutPanel1.GetControlFromPosition(6, i) == null)
+                    {
+                        this.tableLayoutPanel1.Controls.Add(makeNewLabel(isPlayerOne), 6, i);
+                        hasControl = true;
+
+                        isPlayerOne = false;
+
+                    }
+                    else
+                    {
+                        i--;
+                    }
                 }
+
+
 
             }
         }
